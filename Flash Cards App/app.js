e82d592a2ffea4b96d419c01fae2099aa39bb6d8
Justinas Janulevicius <janulevicius.justinas@gmail.com>
2015-10-23 11:44:42 +0000

    Changes from Screen Builder

--- Flash Cards App/app.js
+++ Flash Cards App/app.js
@@ -1,12 +1,12 @@
-(function() {
+(function () {
     // store a reference to the application object that will be created
     // later on so that we can use it if need be
     var app = {
         data: {}
     };
 
-    var bootstrap = function() {
-        $(function() {
+    var bootstrap = function () {
+        $(function () {
             app.mobileApp = new kendo.mobile.Application(document.body, {
 
                 // you can change the default transition (slide, zoom or fade)
@@ -23,7 +23,7 @@
 
     if (window.cordova) {
         // this function is called by Cordova when the application is loaded by the device
-        document.addEventListener('deviceready', function() {
+        document.addEventListener('deviceready', function () {
             // hide the splash screen as soon as the app is ready. otherwise
             // Cordova will wait 5 very long seconds to do it for you.
             if (navigator && navigator.splashscreen) {
@@ -31,13 +31,13 @@
             }
 
             var element = document.getElementById('appDrawer');
-            if (typeof(element) != 'undefined' && element != null) {
+            if (typeof (element) != 'undefined' && element != null) {
                 if (window.navigator.msPointerEnabled) {
-                    $("#navigation-container").on("MSPointerDown", "a", function(event) {
+                    $("#navigation-container").on("MSPointerDown", "a", function (event) {
                         app.keepActiveState($(this));
                     });
                 } else {
-                    $("#navigation-container").on("touchstart", "a", function(event) {
+                    $("#navigation-container").on("touchstart", "a", function (event) {
                         app.keepActiveState($(this));
                     });
                 }
@@ -57,14 +57,24 @@
 
     window.app = app;
 
-    app.isOnline = function() {
+    app.isOnline = function () {
         if (!navigator || !navigator.connection) {
             return true;
         } else {
             return navigator.connection.type !== 'none';
         }
     };
+    
 }());
 
 // START_CUSTOM_CODE_kendoUiMobileApp
+if(app.user) {
+    var loggedUser = app.user.UserName;
+}
+else {
+    var loggedUser = "Anonymous";
+}
+indexViewModel = kendo.observable({ user: loggedUser});
+var index = new kendo.View('loggedUser', {model:indexViewModel, evalTemplate: true });
+console.log(indexViewModel.user);
 // END_CUSTOM_CODE_kendoUiMobileApp
